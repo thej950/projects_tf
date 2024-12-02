bd2cc6f9bd28eab637261afdc5b9e8a5e12cacb4
thej <thej.devops01@gmail.com>
2024-12-02 14:07:59 +0530

    a

--- Projects/ansible_cluster/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "cidr" {
-  default = "10.0.0.0/16"
-}
-
--- Projects/ansible_cluster/workspace/controller.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-apt update -y
-apt install -y software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt update -y
-apt install -y ansible
-
